48394cfe716a3cc1b67afec82788fda5036bcdfe
turnerd18 <turnerd18@gmail.com>
2013-10-26 15:42:47 -0500

    including game manager for handing transition to fps scene

---
+++ Assets/FPS/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿/************************************************************************************
+ *
+ * Filename :   GameManager.cs
+ * Content  :   General level scripting, score tracking, game start and end scripting
+ * Expects  :   (Probably) an OVRPlayerController, tagged enemies
+ * Authors  :   Devin Turner
+ * 
+************************************************************************************/
+
+using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	// Inspector variables
+	
+	
+	// Private variables
+	
+	
+	void Awake () {
+        // Fade in the camera
+        CameraFade.StartAlphaFade(Color.black, true, 4.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
