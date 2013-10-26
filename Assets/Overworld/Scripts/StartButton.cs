4284cc09bd12108fccfe369b4e82b5d0b6b4bab6
turnerd18 <turnerd18@gmail.com>
2013-10-26 15:26:23 -0500

    scripts for scene transition with camera fade in and out

---
+++ Assets/Overworld/Scripts/StartButton.cs
@@ -0,0 +1,29 @@
+﻿/************************************************************************************
+ *
+ * Filename :   StartButton.cs
+ * Content  :   
+ * Expects  :   
+ * Authors  :   
+ * 
+************************************************************************************/
+
+using UnityEngine;
+using System.Collections;
+
+public class StartButton : MonoBehaviour {
+
+	// Inspector variables
+    public string sceneName;
+	
+	// Private variables
+	
+	
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Overworld/Scripts/StartGame.cs
@@ -0,0 +1,42 @@
+﻿/************************************************************************************
+ *
+ * Filename :   StartGame.cs
+ * Content  :   Start arcade game the player is looking at
+ * Expects  :   Arcade machine with "StartButton" tag and StartButton script
+ * Authors  :   Devin Turner
+ * 
+************************************************************************************/
+
+using UnityEngine;
+using System.Collections;
+
+public class StartGame : MonoBehaviour {
+
+	// Inspector variables
+    public float minDistanceToStartGame = 1.0f;
+	
+	// Private variables
+    private Transform cameraPos;
+	
+	void Start () {
+        cameraPos = Camera.main.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Player presses fire1 button
+        if (Input.GetButtonDown("Fire1"))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(cameraPos.position, cameraPos.forward, out hit))
+            {
+                if ((hit.transform.gameObject.tag == "StartButton") && (hit.distance < minDistanceToStartGame))
+                {
+                    StartButton sb = hit.transform.gameObject.GetComponent("StartButton") as StartButton;
+                    // Fade out the camera and load the game level based on the machine they're looking at
+                    CameraFade.StartAlphaFade(Color.black, false, 2.0f, 2.0f, () => { Application.LoadLevel(sb.sceneName); });
+                }
+            }
+        }
+	}
+}
