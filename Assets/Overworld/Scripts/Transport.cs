1f8a78ac1fa906a12fe733bd3edfced924c05d60
turnerd18 <turnerd18@gmail.com>
2013-10-30 16:45:15 -0500

    Scene transition works and Ryan's transport script has been added, though it still needs to be debugged.

---
+++ Assets/Overworld/Scripts/Transport.cs
@@ -0,0 +1,35 @@
+﻿/************************************************************************************
+ *
+ * Filename :   Transport.cs
+ * Content  :   
+ * Expects  :   
+ * Authors  :   Ryan Copeland
+ * 
+************************************************************************************/
+
+using UnityEngine;
+using System.Collections;
+
+public class Transport : MonoBehaviour {
+
+    // Inspector variables
+    public float maxDistance = 40.0f;
+
+    // Private variables
+    private Transform arcade;
+
+	// Use this for initialization
+	void Start () 
+	{
+        arcade = GameObject.FindGameObjectWithTag("Arcade").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{		
+		float distance = Vector3.Distance (transform.position, arcade.position);
+
+        if (distance > maxDistance)
+		    transform.position = new Vector3(-transform.position.x, transform.position.y, -transform.position.z);
+	}
+}
