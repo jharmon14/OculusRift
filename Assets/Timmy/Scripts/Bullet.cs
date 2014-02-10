9124f3f0467b4263c82fd2cd85fb2c4bab6e2d2e
Jon Harmon <jonathanmharmon@gmail.com>
2014-02-10 09:50:27 -0600

    working on flipping character when change directions

---
+++ Assets/Timmy/Scripts/Bullet.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Debug.Log("Hit enemy");
+        }
+        Destroy(transform.parent.gameObject);
+        //For Enemy bullets
+        if (collision.gameObject.tag == "Player")
+        {
+            PlayerScript p = collision.gameObject.GetComponent<PlayerScript>();
+            p.GotShot();
+        }
+    }
+
+}
---
+++ Assets/Timmy/Scripts/PlayerMovement.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public GameObject player;
+    public bool paused;
+    public GameObject Manager;
+
+    private bool direction = true;
+
+    // Use this for initialization
+    void Start()
+    {
+        Manager = GameObject.Find("GameManager");
+        player = GameObject.Find("Player");
+    }
+
+    void OnLevelWasLoaded(int level)
+    {
+        Manager = GameObject.Find("GameManager");
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        paused = Manager.GetComponent<GameManager>().paused;
+        if (paused)
+            return;
+
+        if (Input.GetKey("d"))
+        {
+            if (!direction)
+            {
+                direction = !direction;
+                player.transform.eulerAngles = new Vector3(0, 180, 0);
+            }
+            transform.Rotate(new Vector3(0, 1, 0), 1);
+        }
+        if (Input.GetKey("a"))
+        {
+            if (direction)
+            {
+                direction = !direction;
+                player.transform.eulerAngles = new Vector3(0, 180, 0);
+            }
+            transform.Rotate(new Vector3(0, 1, 0), -1);
+        }
+
+    }
+}
