d4fbcd00476f8afe0e32f951606201ba516dbd56
Ryan <RoughRaptors@gmail.com>
2014-02-18 15:01:58 -0600

    Updated shooting, does not work when changing directions or looking at player

---
+++ Assets/Timmy/BulletRotation.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletRotation : MonoBehaviour 
+{
+
+    public bool direction = true;
+    public EnemyController enemyController;
+	// Use this for initialization
+	void Start () 
+	{
+        //enemyController = GameObject.Find("PlayerParent").GetComponent<EnemyController>();
+		//enemyController = transform.parent.gameObject.GetComponent<EnemyController>();
+        //direction = enemyController.direction;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+        if (direction)
+            transform.Rotate(new Vector3(0, 1.5f, 0));
+		
+        else
+            transform.Rotate(new Vector3(0, -1.5f, 0));
+	}
+}
--- Assets/Timmy/EnemyController.cs
+++ Assets/Timmy/EnemyController.cs
@@ -11,7 +11,10 @@ public class EnemyController : MonoBehaviour
 	private GameObject bulletParent, bullet;
 	
     public float moveSpeed = 1.0f;
+	public bool direction = true;
 	public GameObject bulletPrefab;
+	public GameObject bulletClockWise;
+    public GameObject bulletCounterClockWise;
 	
 	// use this to change how far it will rotate the game object on an enemy by enemy basis (higher = larger rotation
 	public float bound = 0;
@@ -25,6 +28,8 @@ public class EnemyController : MonoBehaviour
         endRotation = transform.rotation;
 		target = GameObject.Find("Player");
 		nextFire = 0;
+		bulletClockWise.SetActive(false);
+        bulletCounterClockWise.SetActive(false);
     }
    
     // Update is called once per frame
@@ -32,21 +37,44 @@ public class EnemyController : MonoBehaviour
 	{		
 		// don't update the enemies unless they are on screen, temporary arbitrary number of 50
 		if(distance < 50)
-		{
+		{			
 			// rotate the parent, calculate distance from player to enemy
-			transform.parent.Rotate(Vector3.up * Time.deltaTime * moveSpeed);
+			transform.parent.Rotate(new Vector3(0, 1, 0), Time.deltaTime * moveSpeed);
 			distance = Vector3.Distance (transform.position, target.transform.position);
 			
 			// if it's going to go out of bounds, reverse the direction
 	        if (Quaternion.Angle(transform.parent.rotation, endRotation) > bound)
+			{
 	            moveSpeed = -moveSpeed;
+				transform.Rotate(0, 180, 0);
+				direction = !direction;
+			}
 			
 			// look at the player if it's close enough
 			if(distance < 10)
 				transform.LookAt(target.transform);
 			
-			// fire 
+			/*
 			if(Time.time > nextFire)// && distance < 20)
+			{
+				nextFire = Time.time + fireRate;
+				Fire();
+			}
+			*/
+			
+			// always destroy the bullet, even if the enemy is out of range of the player
+			// redunant but necessary
+			if(Time.time > nextFire)
+			{
+				if(bullet != null)
+				{
+					Destroy(bullet);
+					Destroy(bulletParent);
+				}
+			}
+			
+			// fire 
+			if(Time.time > nextFire && distance < 20)
 			{
 				// destroy previous bullet
 				if(bullet != null)
@@ -67,9 +95,43 @@ public class EnemyController : MonoBehaviour
 				bullet.transform.parent = bulletParent.transform;				
 			}
 			
-			// rotate the bullet's parent and check for collision
-			if(bulletParent != null)
+			// rotate the bullet's parent based on the direction its facing
+			if(bulletParent != null && direction == true)
 				bulletParent.transform.Rotate(Vector3.up * Time.deltaTime * Mathf.Abs(moveSpeed * 3));
+			
+			else if(bulletParent != null && direction == false)
+				bulletParent.transform.Rotate(Vector3.up * Time.deltaTime * -(Mathf.Abs(moveSpeed * 3)));	
+				
+			
 		}
+	}
+	
+	/*
+	void Fire()
+    {
+        GameObject clone;
+        if (direction)
+        {
+            bulletClockWise.SetActive(true);
+            //clone = Instantiate(bulletClockWise, 
+                //new Vector3(bulletClockWise.transform.position.x, transform.position.y -.2f, bulletClockWise.transform.position.z), 
+                //transform.rotation) as GameObject;
+			clone = (GameObject)Instantiate(bulletClockWise, bulletClockWise.transform.position, bulletClockWise.transform.rotation);
+            bulletClockWise.SetActive(false);
+        }
+        else
+        {
+            bulletCounterClockWise.SetActive(true);
+            //clone = Instantiate(bulletCounterClockWise, 
+                //new Vector3(bulletCounterClockWise.transform.position.x, transform.position.y - .2f, bulletCounterClockWise.transform.position.z), 
+                //transform.rotation) as GameObject;
+			clone = (GameObject)Instantiate(bulletCounterClockWise, bulletCounterClockWise.transform.position, bulletCounterClockWise.transform.rotation);
+            bulletCounterClockWise.SetActive(false);
+        }
+        Destroy(clone, 1);
     }
+    */
+    
+    
 }
+
