1f779521517a5d7f9483e766ae57e8f8c76f5832
turnerd18 <turnerd18@gmail.com>
2014-04-23 12:39:37 -0500

    game will now play at start. cone has issues though.

--- Assets/Misconduct/Scripts/ConeTracker.cs
+++ Assets/Misconduct/Scripts/ConeTracker.cs
@@ -4,60 +4,68 @@ using System.Collections;
 public class ConeTracker : MonoBehaviour {
 	
 	private bool paused;
-	public int suspicionLevel = 0; 
+	public int suspicionLevel = 0;
 	private Transform coneInside;
 	private Transform coneOutside;
-	
+
 	// Use this for initialization
-	void Start () {
-        //manager = GameObject.Find("GameManager");
-        //managerScript = manager.GetComponent<GameManager>();
+	void Awake()
+	{
+		//manager = GameObject.Find("GameManager");
+		//managerScript = manager.GetComponent<GameManager>();
 		coneInside = GameObject.Find("Cone_Inside").transform;
 		coneOutside = GameObject.Find("Cone_Outside").transform;
 	}
-	
-	void OnTriggerEnter(Collider trigger) {
+
+	void OnTriggerEnter(Collider trigger)
+	{
 		/*
         if (paused)
         {
             return;
         }*/
-        if(trigger.gameObject.tag == "Cone"){;
+		if (trigger.gameObject.tag == "Cone")
+		{
 			// Change material to reflect Player action
 			Debug.Log("ENTERED!!?!?!?!?!??!");
 			coneInside.transform.GetComponent<MeshRenderer>().material.color = Color.yellow;
 			coneOutside.transform.GetComponent<MeshRenderer>().material.color = Color.yellow;
-			
+
 		}
-    }
-	
-	void OnTriggerStay(Collider trigger){
+	}
+
+	void OnTriggerStay(Collider trigger)
+	{
 		/*
         if (paused)
         {
             return;
         }*/
-		if(trigger.gameObject.tag == "Cone"){
+		if (trigger.gameObject.tag == "Cone")
+		{
 			// Keep incrementing suspicion if Player is actin' a fool
 			// suspicionLevel++;
 		}
 	}
-	
-	void OnTriggerExit(Collider trigger){
-		if(trigger.gameObject.tag == "Cone"){;
+
+	void OnTriggerExit(Collider trigger)
+	{
+		if (trigger.gameObject.tag == "Cone")
+		{
+			;
 			// Change material to reflect Player action
 			coneInside.transform.GetComponent<MeshRenderer>().material.color = Color.green;
 			coneOutside.transform.GetComponent<MeshRenderer>().material.color = Color.green;
-			
+
 		}
 	}
-	
+
 	/*
 	void Update(){
-        paused = managerScript.paused;
-        if (paused)
-        {
-            return;
-        }	
+				paused = managerScript.paused;
+				if (paused)
+				{
+						return;
+				}	
 	}*/
 }
