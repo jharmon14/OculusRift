ea01b72ff653aebcd74168fbf0e7de421c953b9d
Craig Lane <lane.craig.m@gmail.com>
2014-04-30 14:08:03 -0500

    SuspicionPanel prefab and its script

--- Assets/Misconduct/Scripts/MisconductManager.cs
+++ Assets/Misconduct/Scripts/MisconductManager.cs
@@ -19,6 +19,9 @@ public class MisconductManager : MonoBehaviour
 	public MisconductPlayerController player;
 	[HideInInspector]
 	public MisconductStudent playerStudent;
+	
+	private UISlider slider;
+	
 
 	private int answersCollected = 0;
 	private int suspicionLevel = 0;
@@ -37,14 +40,27 @@ public class MisconductManager : MonoBehaviour
 
 	}
 	
+	void Awake(){
+		slider = GameObject.Find("SuspicionProgressBar").GetComponent<UISlider>();
+	}
+	
 	public void increaseSuspicion(int val = 1){
 		suspicionLevel += val;
+		
+		slider.sliderValue = suspicionLevel/100.0f;
+		
+		// Filled the bar
+		if (slider.sliderValue >= 1.0f){
+			Debug.Log("You got caught!!!");
+		}
 	}
 	
 	public void reduceSuspicion(int val = 1){
 		suspicionLevel -= val;
 		if(suspicionLevel < 0)
 			suspicionLevel = 0;
+		
+		slider.sliderValue = suspicionLevel/100.0f;
 	}
 
 	public void collectAnswer()
