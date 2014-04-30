bc6ea9f220a7e4a31a2f7cd615bbbd334f913e1d
Craig Lane <lane.craig.m@gmail.com>
2014-04-30 12:29:16 -0500

    Cone detects player angle difference.  Updates at fixed time while triggerStay.

--- Assets/Misconduct/Scripts/ConeTracker.cs
+++ Assets/Misconduct/Scripts/ConeTracker.cs
@@ -8,17 +8,33 @@ public class ConeTracker : MonoBehaviour {
 	private Transform coneInside;
 	private Transform coneOutside;
 	private int triggerCatch = 0;
+	
+	private GameObject teacher;
+	private Transform player;
 
 	private GameObject manager;
     private MisconductManager managerScript;
+	private MisconductTeacherMovement teacherMovement;
+	
+	private float startTime = 0.0f;
+	private float deltaTime = 0.0f;
+	
+	public float suspicionTimeTick = 0.25f;
+	public float playerSuspicionAngle = 10.0f; 
 
 	// Use this for initialization
 	void Awake()
 	{
 		manager = GameObject.Find("MisconductManager");
         managerScript = manager.GetComponent<MisconductManager>();
+		
+		player = GameObject.Find("Player").transform;
+
 		coneInside = GameObject.Find("Cone_Inside").transform;
 		coneOutside = GameObject.Find("Cone_Outside").transform;
+
+		teacher = GameObject.Find ("Teacher");
+		teacherMovement = teacher.GetComponent<MisconductTeacherMovement>();
 	}
 
 	void OnTriggerEnter(Collider trigger)
@@ -29,24 +45,45 @@ public class ConeTracker : MonoBehaviour {
 			// Change material to reflect Player action
 			updateConeColor(Color.yellow);
 			
-			// Stop Pathing and Head Movement
+			// Check if player is supicious
+			if(isPlayerSuspicious()){
+				setTeacherPath(false);
+				
+				// Time since game started
+				startTime = Time.time;
+
+			}
 
 		}
 	}
 
 	void OnTriggerStay(Collider trigger)
 	{
-		/*
-        if (paused)
-        {
-            return;
-        }*/
+
 		if (trigger.gameObject.tag == "Cone")
 		{
-			updateConeColor(Color.red);
+			if(isPlayerSuspicious()){
+				
+				deltaTime = Time.time - startTime;
 
-			// Keep incrementing suspicion if Player is actin' a fool
-			managerScript.increaseSuspicion();
+				if(deltaTime > suspicionTimeTick){
+					// Keep incrementing suspicion if Player is actin' a fool
+					managerScript.increaseSuspicion();
+					Debug.Log (deltaTime);
+					
+					// Reset Time
+					startTime = Time.time;
+					deltaTime = 0;
+				}
+				
+				
+				updateConeColor(Color.red);
+				
+			} else {
+				updateConeColor(Color.yellow);
+				setTeacherPath(true);
+	
+			}			
 		}
 	}
 
@@ -54,10 +91,10 @@ public class ConeTracker : MonoBehaviour {
 	{
 		if (trigger.gameObject.tag == "Cone")
 		{
+			Debug.Log ("Leaving Trigger");
 			// Change material to reflect Player action
 			updateConeColor(Color.green);
-			
-			// Start Pathing and Head Movement
+
 		}
 	}
 	
@@ -65,14 +102,28 @@ public class ConeTracker : MonoBehaviour {
 		coneInside.transform.GetComponent<MeshRenderer>().material.color = color;
 		coneOutside.transform.GetComponent<MeshRenderer>().material.color = color;
 	}
+	
+	void setTeacherPath(bool setBool){
+		if(setBool){
+			teacherMovement.enabled = true;
+		} else {
+			teacherMovement.enabled = false;
+		}
+	}
+	
+	bool isPlayerSuspicious(){
+		Quaternion targetRotation = Quaternion.Euler(0, 0, 0);
+		float angle = Quaternion.Angle(player.rotation, targetRotation);
+		
+		if(angle > playerSuspicionAngle){
+			return true;
+		} else {
+			return false;
+		}
+		
+	}
 
 	void Update(){
-		/*
-				paused = managerScript.paused;
-				if (paused)
-				{
-						return;
-				}
-		*/
+
 	}
 }
