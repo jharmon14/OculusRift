7d58582a8645cc6c86462a3f068ab103e4fad97b
Ryan Copeland <RoughRaptors@Gmail.com>
2014-05-05 17:31:46 -0500

    Added new FPS and Timmy video

--- Assets/Overworld/Scripts/PlayVideo.cs
+++ Assets/Overworld/Scripts/PlayVideo.cs
@@ -2,16 +2,13 @@
 using System.Collections;
 
 public class PlayVideo : MonoBehaviour
-{
-
+{	
 	// Use this for initialization
 	void Start () 
 	{
 		MovieTexture movie = renderer.material.mainTexture as MovieTexture;
-		audio.clip = movie.audioClip;
-		
+		movie.loop = true;
 		movie.Play();
-		//audio.Play();
 	}
 	
 	// Update is called once per frame
